6f6257a24dfc6ad8a9229cf5aca1fe4ed1c8d424
Ásdís Karen <asdisw17@ru.is>
2020-05-06 17:24:15 +0000

    sölusíða

--- product/views.py
+++ product/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+products = [
+    {'name': 'Tölvuleikur 1', 'price': 1000},
+    {'name': 'Tölvuleikur 2', 'price': 1000},
+    {'name': 'Tölvuleikur 3', 'price': 1000},
+    {'name': 'Leikjatölva 1', 'price': 5000},
+    {'name': 'Leikjatölva 2', 'price': 5000},
+
+]
+
 # Create your views here.
 def index(request):
-    return render(request, 'product/index.html')
+    return render(request, 'product/index.html', context={'products': products})
+
+def info_index(request):
+    return render(request, 'product/info.html')
+
+
