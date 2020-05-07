f962906dc468f6f58a391c85d46636b0f6c101a0
Ásdís Karen <asdisw17@ru.is>
2020-05-07 18:32:47 +0000

    leitarsaga

--- user/views.py
+++ user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
+def search_history(request):
+    return render(request, 'user/search_history.html')
