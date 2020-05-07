994ef2934ffa837b9f93943b6e7c9087faf43967
sirunnar <sirunnar@gmail.com>
2020-05-07 21:10:28 +0000

    Meira bara user HTML

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'user_index'),
     path('searchhistory/', views.search_history, name = 'search_history'),
+    path('new_user/', views.new_user, name = 'new_user'),
+    path('user_area/', views.user_area, name = 'user_area'),
+    path('user_area/change_payment/', views.change_payment, name = 'change_payment'),
 ]
+
