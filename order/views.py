553e48a77cb894bbd4664546d28eeccd4e2be853
Ásdís Karen <asdisw17@ru.is>
2020-05-10 21:48:00 +0000

    some problems

--- order/views.py
+++ order/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from user.forms.personal_info import PersonalInfo
+from user.forms.personal_info import AddressInfo
+from user.forms.payment_info import PaymentInfo
+from user.models import Customer
+from user.models import Card
+
+
 
 # Create your views here.
 def index(request):
     return render(request, 'order/index.html')
+
+
+def payment(request):
+    user = User.objects.filter(id=request.user.id).first()
+    address = Customer.objects.filter(id=request.user.id).first()
+    paymentInfo = Card.objects.filter(id=request.user.id).first()
+
+    if request.method == 'POST':
+        print('order/payment - POST')
+
+    return render(request, 'order/payment.html', {
+        'form': PersonalInfo(instance=user),
+        'address': AddressInfo(instance=address),
+        'carddetails': PaymentInfo(instance=paymentInfo)
+    })
--- product/models.py
+++ product/models.py
@@ -68,4 +68,3 @@ class SearchHistory(models.Model):
 
 
 
-
