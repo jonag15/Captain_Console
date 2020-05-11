64edb09731cb42c18bc389ff08e596f00d617dee
Jón Ágúst Guðmundsson <jonag15@ru.is>
2020-05-11 12:59:38 +0000

    karfa næstum komin vantar bara ajax tengingu

--- order/views.py
+++ order/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from user.forms.personal_info import PersonalInfo
@@ -5,11 +6,21 @@ from user.forms.personal_info import AddressInfo
 from user.forms.payment_info import PaymentInfo
 from user.models import Customer
 from user.models import Card
-
+from product.models import Product
 
 
 # Create your views here.
 def index(request):
+    if 'order_list' in request.GET:
+        order_list = request.GET['order_list']
+        products = [ {
+            'id': x.id,
+            'name': x.name,
+            'price': x.price,
+            'firstImage': x.productimage_set.first().image
+        } for x in Product.objects.filter(pk__in=order_list) ]
+        return JsonResponse({'data': products})
+
     return render(request, 'order/index.html')
 
 
--- static/js/current_order.js
+++ static/js/current_order.js
@@ -6,22 +6,71 @@ function getCurrentOrder() {
     for (i = 0; i <= localStorage.length-1; i++) {
         key = localStorage.key(i);
         value = localStorage.getItem(key);
-        if (key == 'name') //til dæmis
-        	 {
-
-        	 	// myndi þá fylla inn nafn í post request hér
-			 }
-    	else //ef að það passar ekki við hitt þá er þetta vara
-			 {
-				//vöru væri bætt við post hér
-		}
     }
 }
 
+$(document).ready(function() {
+	if (top.location.pathname === '/order/') {
+		console.log( "ready!" );
+		var orderList = [];
+		var i = 0;
+		for (i = 0; i <= localStorage.length - 1; i++) {
+			orderList.push(parseInt(localStorage.key(i)));
+		};
+		console.log( orderList );
+
+		$.ajax( {
+			url: '/order?order_list=' + orderList,
+			type: 'GET',
+			success: function (resp) {
+				console.log( "success!" );
+				var newHtml = resp.data.map(d => {
+					var count = localStorage.getItem((d.id).toString())
+					return `<div class="single_product">
+							  <a href="/product/${d.id}">
+								<tr>
+								  <th scope="row">
+									<div class="p-2">
+									  <img src="${d.firstImage}" alt="Picture of product" class="img-fluid rounded shadow-sm gen_pic">
+									  <div class="ml-3 d-inline-block align-middle">
+										<h5 class="mb-0"><a href="#" class="text-dark d-inline-block">${d.name}</a></h5>
+									  </div>
+									</div>
+								  </th>
+								  <td class="align-middle"><strong>${d.price}</strong></td>
+								  <td class="align-middle">
+									  <div class='order_count'>
+										  <button class='down_count btn btn-secondary btn-sm' title='Down'><i class='fa fa-minus'></i></button>
+										  <input class='counter' type="text" value="${count}" />
+										  <button class='up_count btn btn-secondary btn-sm' title='Up'><i class='fa fa-plus'></i></button>
+										</div>
+								  </td>
+								  <td class="align-middle trash-can"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+								</tr>
+							  </a>
+							</div>`
+				});
+				$('.order_products').html(newHtml.join(''));
+			},
+			error: function (xhr, status, error) {
+				// TODO: error message
+				console.error(error)
+
+			}
+
+		})
+	}
+});
+
 function AddItem(product_id) {
 	var id = product_id;
-	var count = document.forms.ShoppingList.data.value;
-	localStorage.setItem(id, count);
+	if (localStorage.getItem(id) == null) {
+		localStorage.setItem(id,1)
+	}
+	else {
+		var count = localStorage.getItem(id);
+		localStorage.setItem(id, parseInt(count)+ 1);
+	}
 }
 
 function ModifyItemCount() {
