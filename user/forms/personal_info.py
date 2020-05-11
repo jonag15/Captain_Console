8276a28356a0e48a99e1b04bb8646a896811ad11
sirunnar <sirunnar@gmail.com>
2020-05-11 09:04:38 +0000

    Sjálfvirk tilfræsla eftir hvort er staff eða customer

--- user/forms/personal_info.py
+++ user/forms/personal_info.py
@@ -7,7 +7,7 @@ class PersonalInfo(ModelForm):
         model = User
         exclude = [ 'id', 'last_login', 'date_joined', 'password', 'is_superuser', 'username', 'is_active', 'is_staff', 'groups', 'user_permissions' ]
         widgets = {
-            'first_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'first_name': widgets.TextInput(attrs={'class': 'form-control', }),
             'last_name': widgets.TextInput(attrs={'class': 'form-control'}),
             'email': widgets.TextInput(attrs={'class': 'form-control'})
         }
