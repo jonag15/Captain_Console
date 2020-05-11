1b46c7e57ff8203cec9f889647e1b7ec3b4b6cf8
sirunnar <sirunnar@gmail.com>
2020-05-11 14:17:23 +0000

    Fegrunaraðgerð á nýskráningu plús bætti við email mv hönnunarskýrslu

--- user/forms/registration.py
+++ user/forms/registration.py
@@ -1,5 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from account.models import account
+from django.contrib.auth.models import User
 
-class RegistrationForm(UserCreationForm):
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
