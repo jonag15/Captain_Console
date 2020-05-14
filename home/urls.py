e79ceeea3137725b4243aa214cadd8ed207810fb
sirunnar <sirunnar@gmail.com>
2020-05-14 21:01:18 +0000

    Villuskilaboð í nýskráningu

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'home_index'),
+    path('product', views.redirecter, name = 'rederecter')
 ]
--- home/views.py
+++ home/views.py
@@ -6,5 +6,9 @@ from django.http import JsonResponse
 def index(request):
     context = {'products': Product.objects.all()}
     if 'search_filter' in request.GET:
-        redirect('/product/', request.GET)
-    return render(request, 'home/index.html')
+        redirect('product_index', request)
+    return render(request, 'home/index.html', context)
+
+
+def redirecter(request):
+    redirect('product_index')
