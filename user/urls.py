2b0242644be704fff005cd103a2ce8241fb83de4
Ásdís Karen <asdisw17@ru.is>
2020-05-15 22:51:41 +0000

    Lagfærsla

--- user/urls.py
+++ user/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('/view_orders/', views.get_orders_to_choose_from, name='view_all_orders'),
-    path('/view_orders/<int:id>', views.get_order_by_id, name='order_by_id')
+
 ]
 
